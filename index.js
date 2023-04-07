66d0d3a04cc621315e94ffd6b039d529fd14188b
Musa <musamaxwell15@gmail.com>
2023-04-07 12:08:52 +0300

    Final Code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8000;
 
 const articles = [];
 const newspapers = [
